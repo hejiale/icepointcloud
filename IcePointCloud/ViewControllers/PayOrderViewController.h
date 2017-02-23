4b2388551d23c52d64571747fb8e5ce03850e06a
hejiale <gerry@icepointcloud.com>
2017-02-23 17:48:00 +0800

    cart

---
+++ IcePointCloud/ViewControllers/PayOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayOrderViewController.h
+//  IcePointCloud
+//
+//  Created by mac on 2017/2/21.
+//  Copyright © 2017年 Doray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayOrderViewController : UIViewController
+
+@end
---
+++ IcePointCloud/ViewControllers/PayOrderViewController.m
@@ -0,0 +1,37 @@
+//
+//  PayOrderViewController.m
+//  IcePointCloud
+//
+//  Created by mac on 2017/2/21.
+//  Copyright © 2017年 Doray. All rights reserved.
+//
+
+#import "PayOrderViewController.h"
+
+@interface PayOrderViewController ()
+
+@end
+
+@implementation PayOrderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
