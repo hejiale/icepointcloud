dfbfd209bc48aaac99189a223319f9ca579c8c6f
hejiale <gerry@icepointcloud.com>
2017-04-14 17:47:48 +0800

    1

---
+++ IcePointCloud/Views/IPCPayOrderEmployeeCell.m
@@ -0,0 +1,28 @@
+//
+//  IPCPayAmountStyleCell.m
+//  IcePointCloud
+//
+//  Created by mac on 2017/3/3.
+//  Copyright © 2017年 Doray. All rights reserved.
+//
+
+#import "IPCPayOrderEmployeeCell.h"
+
+
+@implementation IPCPayOrderEmployeeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+
+
+@end
