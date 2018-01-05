06b93fc23940e79cf7612242a6025c635f20ebd1
hejiale <gerry@icepointcloud.com>
2018-01-05 17:44:49 +0800

    新增修改客户信息页面

--- IcePointCloud/Views/EditCustomer/UpdateCustomer/IPCUpdateCustomerView.m
+++ IcePointCloud/Views/EditCustomer/UpdateCustomer/IPCUpdateCustomerView.m
@@ -8,14 +8,45 @@
 
 #import "IPCUpdateCustomerView.h"
 
+@interface IPCUpdateCustomerView()
+
+@property (weak, nonatomic) IBOutlet UITextField *customerNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextFiel;
+@property (weak, nonatomic) IBOutlet UITextField *ageTextField;
+@property (weak, nonatomic) IBOutlet UITextField *birthdayTextField;
+@property (weak, nonatomic) IBOutlet UIButton *maleButton;
+@property (weak, nonatomic) IBOutlet UIButton *femaleButton;
+@property (weak, nonatomic) IBOutlet UITextField *customerTypeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *encryptedPhoneTextField;
+@property (weak, nonatomic) IBOutlet UIView *editContentView;
+
+@end
+
 @implementation IPCUpdateCustomerView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame DetailCustomer:(IPCDetailCustomer *)customer UpdateBlock:(void (^)(NSString *))update
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        UIView * view = [UIView jk_loadInstanceFromNibWithName:@"IPCUpdateCustomerView" owner:self];
+        [view setFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
+        [self addSubview:view];
+        
+        [self.editContentView.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if ([obj isKindOfClass:[UITextField class]]) {
+                UITextField * textFiedld = (UITextField *)obj;
+                [textFiedld addBottomLine];
+            }
+        }];
+        
+        [self.birthdayTextField setRightButton:self Action:@selector(showDatePickerAction) OnView:self.editContentView];
+        [self.customerTypeTextField setRightButton:self Action:@selector(selectCustomTypeAction) OnView:self.editContentView];
+        
+        [[IPCCustomerManager sharedManager] queryCustomerType];
+        [self.customerTypeTextField setText: @"自然进店"];
+    }
+    return self;
 }
-*/
 
 @end
